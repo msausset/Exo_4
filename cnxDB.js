de709f9059707bb5d8afcb28341f93fbbe5c5525
msausset <74968831+msausset@users.noreply.github.com>
2022-10-06 17:32:34 +0200

    Add files via upload

---
+++ cnxDB.js
@@ -0,0 +1,34 @@
+var MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb+srv://mangeonsensemble:projet_pim_estiam@cluster0.svlza.mongodb.net/test",
+
+  (err, client) => {
+    // Client returned
+
+    var db = client.db("test");
+  }
+);
+
+MongoClient.connect(
+  "mongodb+srv://mangeonsensemble:projet_pim_estiam@cluster0.svlza.mongodb.net/test",
+
+  async function (err, client) {
+    if (err) throw err;
+    var db = client.db("test");
+    var db = client.db("test");
+    var users = db.collection("test");
+
+    const count = await users.countDocuments({});
+
+    console.log(`Nombre de documents : ${count}`);
+
+    db.collection("test")
+      .find({})
+      .toArray(function (err, docs) {
+        if (err) throw err;
+        else console.log(docs);
+        client.close();
+      });
+  }
+);
